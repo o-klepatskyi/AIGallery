ae2641796d3a1d3f33057541db9fdcbec81edd3d
Oleh Klepatskyi <o.klepatskyi@gmail.com>
2024-04-24 08:11:19 +0300

    Initial commit

---
+++ src/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using System.Diagnostics;
+
+namespace AIGallery
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+        }
+    }
+
+}
